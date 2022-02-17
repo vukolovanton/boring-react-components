7e93e5eccf742e14bc793068f5e0de8f52edf9bd
Anton Vukolov <vukolovanton92@gmail.com>
2022-02-17 23:02:01 +0400

    Configure scripts for npm publish

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+import { Stepper } from '@/components/Stepper';
+import { SplitButton } from '@/components/SplitButton';
+import { Spinner } from './components/Spinner';
+import { ProgressBar } from './components/ProgressBar';
+
+export { Stepper, SplitButton, Spinner, ProgressBar };
--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,28 @@
 import { defineConfig } from 'vite';
+import dts from 'vite-dts';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 
 export default defineConfig({
-	plugins: [react()],
+	plugins: [react(), dts()],
 	resolve: {
 		alias: {
 			'@': path.resolve(__dirname, './src'),
+		},
+	},
+	build: {
+		lib: {
+			entry: path.resolve(__dirname, 'src/index.ts'),
+			name: 'useless-react-components',
+			fileName: (format) => `useless-react-components.${format}.js`,
+		},
+		rollupOptions: {
+			external: ['react'],
+			output: {
+				globals: {
+					react: 'React',
+				},
+			},
 		},
 	},
 });
